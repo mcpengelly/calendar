db005a6c30d6e69a85367354a2dee4d338c9d5b8
mcpengelly <pengelly.mat@gmail.com>
2018-02-22 19:46:45 -0500

    [feat/componentization] break out timesheets into smart and dumb component structure

--- src/App.js
+++ src/App.js
@@ -3,55 +3,57 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'; // wraps the
 import RaisedButton from 'material-ui/RaisedButton';
 import './App.css';
 
+import TimesheetForm from './components/TimesheetForm';
+
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: []
-    };
-  }
-  render() {
-    let posts = this.state.posts.map((post) => {
-      return (<li id={post.id}>{post.body}</li>);
-    });
+	constructor() {
+		super();
+		this.state = {
+			posts: []
+		};
+	}
+	render() {
+		//example list
+		let posts = this.state.posts.map(post => {
+			return <li id={post.id}>{post.body}</li>;
+		});
 
-    return (
-      <MuiThemeProvider>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">...</h1>
-          </header>
-          <p className="App-intro" />
+		return (
+			<MuiThemeProvider>
+				<div className="App">
+					<p className="App-intro" />
 
-          <input type="button" onClick={this.onTestClick.bind(this)} value="testAPI" />
-          <RaisedButton label="material-ui Button example" onClick={this.onMaterialUiButtonClick.bind(this)} />
-          <input type="textbox" />
-          <br />
-          <ul>
-            {posts}
-          </ul>
-        </div>
+					<input type="button" onClick={this.onTestClick.bind(this)} value="testAPI" />
+					<RaisedButton
+						label="Raised Button example"
+						onClick={this.onMaterialUiButtonClick.bind(this)}
+					/>
+					<input type="textbox" />
+					<br />
+					<ul>{posts}</ul>
 
-      </MuiThemeProvider>
-    );
-  }
+					<TimesheetForm />
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 
-  onTestClick() {
-    console.log('onTestClick() check console for results');
-    this.fetchPosts();
-  }
+	onTestClick() {
+		console.log('onTestClick() click handler, check console for results');
+		this.fetchPosts();
+	}
 
-  async fetchPosts() {
-    const posts = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await posts.json();
-    console.log(data);
-    this.setState({ posts: data });
-  }
+	// example fetch
+	async fetchPosts() {
+		const posts = await fetch('https://jsonplaceholder.typicode.com/posts');
+		const data = await posts.json();
+		console.log(data);
+		this.setState({ posts: data });
+	}
 
-  onMaterialUiButtonClick(){
-    console.log('onMaterialUiButtonClick()');
-
-  }
+	onMaterialUiButtonClick() {
+		console.log('onMaterialUiButtonClick()');
+	}
 }
 
 export default App;
---
+++ src/components/TimesheetList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+//a "dumb" render component that simply takes input from props and renders it accordingly
+class TimesheetList extends Component {
+	render() {
+		const sheets = this.props.timesheets;
+		let timesheets = sheets.map(sheet => {
+			return (
+				<li key={sheet.id}>
+					hours: {sheet.hours} on date: {sheet.day}
+				</li>
+			);
+		});
+
+		return (
+			<div>
+				<ul>{timesheets}</ul>
+			</div>
+		);
+	}
+}
+
+export default TimesheetList;
