0043818a6035123ee14ff20eeca02daa9cc7279c
mcpengelly <pengelly.mat@gmail.com>
2018-02-23 16:36:13 -0500

    [feat/githooks] add githooks for prettifying, also prettify existing code

--- src/components/TimesheetList.js
+++ src/components/TimesheetList.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 
 //a "dumb" render component that simply takes input from props and renders it accordingly
 class TimesheetList extends Component {
-	render() {
-		const sheets = this.props.timesheets;
-		let timesheets = sheets.map(sheet => {
-			return (
-				<li key={sheet.id}>
-					hours: {sheet.hours} on date: {sheet.day}
-				</li>
-			);
-		});
+  render() {
+    const sheets = this.props.timesheets;
+    let timesheets = sheets.map(sheet => {
+      return (
+        <li key={sheet.id}>
+          hours: {sheet.hours} on date: {sheet.day}
+        </li>
+      );
+    });
 
-		return (
-			<div>
-				<ul>{timesheets}</ul>
-			</div>
-		);
-	}
+    return (
+      <div>
+        <ul>{timesheets}</ul>
+      </div>
+    );
+  }
 }
 
 export default TimesheetList;
